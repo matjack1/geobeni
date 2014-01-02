bf9d55c25170ff62b9db7b85c9b02b8d6167a300
Matteo Giaccone <matteo.giaccone@welaika.com>
2014-01-02 16:56:39 +0100

    Better term legend code

--- wp-content/plugins/geo-mashup-custom/custom.js
+++ wp-content/plugins/geo-mashup-custom/custom.js
@@ -1,17 +1,13 @@
 GeoMashup.addAction( 'objectIcon', function( properties, object ) {
   object.icon.image = properties.template_url_path + '/assets/images/' + object.tipologia_icon;
   object.icon.iconSize = [ 25, 36 ];
+
+  // add icon to term_properties for legend retrieval
+  properties.term_properties.stato.terms[object.terms.stato].icon = object.icon
 } );
 
 GeoMashup.addAction( 'taxonomyLegendEntry', function($key, $def, taxonomy, term_id) {
-  var icons = {
-    '10': 'giallo.png',
-    '9': 'rosso.png',
-    '8': 'verde.png',
-    '7': 'blu.png'
-  };
-
-  var icon = icons[term_id];
-  jQuery('img', $key).attr('src', '/wp-content/themes/geoblog/assets/images/' + icon);
+  var icon = GeoMashup.opts.term_properties.stato.terms[term_id].icon;
+  jQuery('img', $key).attr('src', icon.image);
 } );
 
