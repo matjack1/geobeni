2c27c2c82b92c94840985bc876a52017274fb226
Matteo Giaccone <matteo.giaccone@welaika.com>
2014-05-17 22:04:03 +0100

    Simple Fields 1.4.7

--- wp-content/plugins/simple-fields/tests/bootstrap.php
+++ wp-content/plugins/simple-fields/tests/bootstrap.php
@@ -1,17 +1,23 @@
 <?php
 
-#echo "WP_TESTS_DIR is: " . getenv( 'WP_TESTS_DIR' );
-#phpinfo();exit;
+ini_set('display_errors','on');
+error_reporting(E_ALL);
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+define( 'WP_TESTS_DIR', getenv( 'WP_TESTS_DIR' ) );
+
+echo "\nWP_TESTS_DIR is " . WP_TESTS_DIR . "\n";
+echo "\ncurrent dir is " . getcwd() . "\n";
+
+require_once dirname( __FILE__ ) . '/../tmp/wordpress-tests/includes/functions.php';
+
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../simple_fields.php';
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
-
+require WP_TESTS_DIR . '/includes/bootstrap.php';
 
 class SimpleFields_TestCase extends WP_UnitTestCase {
 	
@@ -23,11 +29,4 @@ class SimpleFields_TestCase extends WP_UnitTestCase {
 
 	}
 
-	/*
-	function plugin() {
-		return CWS_PageLinksTo::$instance;
-	}
-	*/
-
 }
-
