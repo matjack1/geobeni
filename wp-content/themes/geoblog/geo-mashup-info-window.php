d6edcfc6b46d1970391f0fe5604e747b577e0237
performingmedialab <performingmedialab@performingmedialab-laptop.(none)>
2014-05-23 18:33:39 +0200

    modificato parzialmente location info
    modifiche allo stile e ai font
    
    Show link in infobubble if present
    
    favicon e footbar tornata colorata
    
    aggiunta favicon e modifica stili su page
    
    tolto favicon
    
    aggiunta favicon
    
    favicon presente su tutte le pagine

--- wp-content/themes/geoblog/geo-mashup-info-window.php
+++ wp-content/themes/geoblog/geo-mashup-info-window.php
@@ -8,8 +8,18 @@ add_filter( 'the_excerpt', array( 'GeoMashupQuery', 'strip_brackets' ) );
     <?php echo wp_get_attachment_image((simple_fields_value("thumbnail")), 'infowindow') ?>
     <h2><a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
     <p><?php echo GeoMashup::location_info(); ?></p>
-    <p><?php echo link_to('Sito Internet', simple_fields_value("website")) ?></p>
-    <p><?php echo link_to('Indirizzo Email', 'mailto:'.simple_fields_value("email")) ?></p>
-  <?php endwhile; ?>
+
+    <?php
+    $website = simple_fields_value("website");
+    $email = simple_fields_value("email");
+
+    if (!empty($website)) { ?>
+      <p><?php echo link_to('Sito Internet', $website) ?></p>
+    <? }
+
+    if (!empty($email)) { ?>
+      <p><?php echo link_to('Indirizzo Email', 'mailto:'.$email) ?></p>
+    <? }
+  endwhile; ?>
 </div>
 
