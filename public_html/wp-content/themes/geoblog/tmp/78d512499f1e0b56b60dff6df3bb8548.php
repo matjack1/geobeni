7db2426a46d7187190edc7fd784c716d5707534f
Matteo Giaccone <matteo@turnintocoders.it>
2019-06-05 22:33:08 +0200

    Remove tmp files

--- public_html/wp-content/themes/geoblog/tmp/78d512499f1e0b56b60dff6df3bb8548.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '/home/matteo/src/geobeni/wp-content/plugins/wordless/vendor/phamlp/haml/HamlHelpers.php';
-?><a href="/">
-<h1 class="logo">
-  Geoblog Beni confiscati in Piemonte
-</h1></a><a href="http://liberapiemonte.it/">
-<div class="libera">
-</div></a>
